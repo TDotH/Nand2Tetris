6eba6f162b5686420c5a2bd4aad6bc849826f15f
Tyde Hashimoto <hasht795@hotmail.com>
2021-01-28 12:32:49 -0800

    Chapter 4 Completion

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,61 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//R0 = counter
+//R1 = White (0) or Black (1)
+//R2 = Bitwise Fill Color
+(MAIN)
+	@KBD
+	D=M
+	@R1
+	D=D-M
+	@KEYBOARD_UP
+	D;JLT
+	@KEYBOARD_DOWN
+	D;JGT
+	@MAIN
+	0;JMP	
+
+(KEYBOARD_DOWN)
+	@R0
+	M=0
+	@R1
+	M=1
+	@R2
+	M=-1
+	@COLOR
+	0;JEQ
+	
+(KEYBOARD_UP)
+	@R0
+	M=0
+	@R1
+	M=0
+	@R2
+	M=0
+	@COLOR
+	0;JEQ
+	
+(COLOR)
+	@R0
+	D=M
+	@8192 //Screen size
+	D=D-A
+	@MAIN //Check loop
+	D;JEQ
+	@R0
+	D=M
+	@SCREEN 
+	D=A+D
+	@R3
+	M=D
+	@R2
+	D=M
+	@R3
+	A=M
+	M=D
+	@R0
+	M=M+1
+	@COLOR
+	0;JMP
